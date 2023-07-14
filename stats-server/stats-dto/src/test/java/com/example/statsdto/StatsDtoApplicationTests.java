68408cf73374130890ae717309230a8c817e87a6
vovasitnikov <vovasitnikov1209@yandex.ru>
2023-07-14 12:25:37 +0400

    скелет программы

---
+++ stats-server/stats-dto/src/test/java/com/example/statsdto/StatsDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.statsdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
