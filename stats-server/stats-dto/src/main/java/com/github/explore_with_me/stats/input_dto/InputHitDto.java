57145db3327d34169a92e9989fb44149217ebfa3
vovasitnikov <vovasitnikov1209@yandex.ru>
2023-07-14 16:11:34 +0400

    исправленияЭ

---
+++ stats-server/stats-dto/src/main/java/com/github/explore_with_me/stats/input_dto/InputHitDto.java
@@ -0,0 +1,20 @@
+package com.github.explore_with_me.stats.input_dto;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InputHitDto {
+
+    private String app;
+    private String uri;
+    private String ip;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
