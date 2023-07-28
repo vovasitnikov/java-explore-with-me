3504269e2fbadf5b3e89b15c04d16ed4373b749f
Vova <vovasitnikov1209@yandex.ru>
2023-07-28 08:48:41 +0300

    убираем лишнее

--- main-server/src/main/java/com/github/explore_with_me/main/MainServiceApp.java
+++ main-server/src/main/java/com/github/explore_with_me/main/MainServiceApp.java
@@ -2,10 +2,8 @@ package com.github.explore_with_me.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
-@ComponentScan({"com.github.explore_with_me.main", "com.github.explore_with_me.stats.client"})
 public class MainServiceApp {
 
     public static void main(String[] args) {
