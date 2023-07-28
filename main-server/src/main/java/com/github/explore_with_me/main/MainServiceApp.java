2279bfa0460f06718e130b5396b852d2288a0281
Vova <vovasitnikov1209@yandex.ru>
2023-07-28 08:53:19 +0300

    спринг не ищет компоненты.откат

--- main-server/src/main/java/com/github/explore_with_me/main/MainServiceApp.java
+++ main-server/src/main/java/com/github/explore_with_me/main/MainServiceApp.java
@@ -2,8 +2,10 @@ package com.github.explore_with_me.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
+@ComponentScan({"com.github.explore_with_me.main", "com.github.explore_with_me.stats.client"})
 public class MainServiceApp {
 
     public static void main(String[] args) {
